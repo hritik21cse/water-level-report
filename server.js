f7d788b57fa49dc982a39fa9ad01a69a6f9b64b3
hritik21cse <hritik21bce7815@gmail.com>
2022-12-04 23:23:50 +0530

    port changes added

--- server.js
+++ server.js
@@ -12,4 +12,6 @@ app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname+'/index.html'))
 })
 
-app.listen(3000)
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`))
