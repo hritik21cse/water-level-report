7778c598ce3fb7767e9e1163f48087ce7d115ed1
hritik21cse <hritik21bce7815@gmail.com>
2022-12-04 17:37:37 +0530

    adding node changes

---
+++ server.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+
+
+console.log('Welcome to Node')
+
+// serve up the static files
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/index.html'))
+})
+
+app.listen(3000)
